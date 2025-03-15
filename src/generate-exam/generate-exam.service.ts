7764a2191bae3120860b1bc2c49d2fe16666605e
abdo20033110 <babelly1996@gmail.com>
2025-03-15 23:05:53 +0300

    regex to extract json

--- src/generate-exam/generate-exam.service.ts
+++ src/generate-exam/generate-exam.service.ts
@@ -13,31 +13,38 @@ export class GenerateExamService {
     });
     return response;
   }
+
   async generateExam(examDto: ExamDto) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    const input = `${examDto.text}
+    give me 4 multiple choice question with the 3 choices for each question
+    and give it in the following JSON structure don't give me anything other than the JSON i asked for not a single dot more
+    { multipleChoice: [{ question : "question text here", choices: ["choices here"], answer : "answer"}] }`;
     const response = await this.huggingFaceService.hf.textGeneration({
       modle: 'google/gemma-3-27b-it',
-      inputs: `${examDto.text}
-        create an exam from the previous text with 4 multiple choice question with the 3 choices for each question  {
-           multipleChoice: [{ question : "question text here", choices: ["choices here"], answer : "answer"}]
-       }`,
+      inputs: input,
     });
-    return response.generated_text.replaceAll('\n', '');
+    return response.generated_text
+      .replaceAll('\n', '')
+      .replaceAll('```', '')
+      .match(/json.*/)?.[0]
+      .substring(4);
 
-    /*     const response = await ollama.chat({
-      model: 'gemma3:1b',
-      messages: [
-        {
-          role: 'user',
-          content: `${examDto.text}
-           باللغة العربية، قم بإنشاء 4 أسئلة من النص السابق باستخدام مخطط json التالي
-            {
-                multipleChoice: [{ question : "ضع هنا نص السؤال", choices: ["ضع هنا الخيارات"], answer : "ضع هنا الجواب"}]
-            }`,
-        },
-      ],
-    });
-    return response.message.content.replaceAll('\n', '');
- */
+    /*    
+    const response = await ollama.chat({
+    model: 'gemma3:1b',
+    messages: [
+      {
+        role: 'user',
+        content: `${examDto.text}
+          باللغة العربية، قم بإنشاء 4 أسئلة من النص السابق باستخدام مخطط json التالي
+          {
+              multipleChoice: [{ question : "ضع هنا نص السؤال", choices: ["ضع هنا الخيارات"], answer : "ضع هنا الجواب"}]
+          }`,
+      },
+    ],
+  });
+  return response.message.content.replaceAll('\n', '');
+    */
   }
 }
